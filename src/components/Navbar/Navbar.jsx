f0d35dbd949cb48e169e927398ecb1595fdbc15e
nikoguerrero <nclguerrero@gmail.com>
2021-09-24 13:07:35 -0300

    Added effect to selected menu in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,27 +3,39 @@ import './Navbar.scss';
 
 const Navbar = (props) => {
   const { setCategory } = props;
+
+
+  let navItem = document.querySelectorAll(".nav__item");
+
+  navItem.forEach((link) => link.addEventListener("click", listActive));
+
+  function listActive() {
+    navItem.forEach((link) => link.classList.remove("nav__item-active"));
+    this.classList.add("nav__item-active");
+  }
+
+
   return (
     <div className="nav">
       <ul className="nav__list">
-        <li className="nav__item">
-          <a onClick={() => setCategory('recommendations')}>
-            <p className="nav__itemlabel">RECOMMENDATIONS</p>
+        <li className="nav__item nav__item-active" onClick={() => setCategory('recommendations')}> 
+          <a>
+            <p>RECOMMENDATIONS</p>
           </a>
         </li>
-        <li className="nav__item">
-          <a onClick={() => setCategory('breakfast')}>
-            <p className="nav__itemlabel">BREAKFAST</p>
+        <li className="nav__item" onClick={() => setCategory('breakfast')}>
+          <a>
+            <p>BREAKFAST</p>
           </a>
         </li>
-        <li className="nav__item">
-          <a onClick={() => setCategory('sandwiches')}>
-            <p className="nav__itemlabel">SANDWICHES</p>
+        <li className="nav__item" onClick={() => setCategory('sandwiches')}>
+          <a>
+            <p>SANDWICHES</p>
           </a>
         </li>
-        <li className="nav__item">
-          <a onClick={() => setCategory('drinks')}>
-            <p className="nav__itemlabel">DRINKS</p>
+        <li className="nav__item" onClick={() => setCategory('drinks')}>
+          <a>
+            <p>DRINKS</p>
           </a>
         </li>
       </ul>
