4ba2c92f3c4aedcd227701e00a3104d01215997a
Niko Guerrero <nclguerrero@gmail.com>
2021-09-25 15:43:35 -0300

    Finished first part of confirm order modal. Added created order to firestore collection

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import './Modal.css';
+import { db, auth, firebase } from '../../firebase';
 
-const Modal = ({ show, onHide }) => {
+const Modal = ({ show, onHide, order, totalOrder }) => {
+  const sendOrder = async (order) => {
+    const orderSent = order.map((item) => {
+      const { id, name, qty } = item;
+      return { id, name, qty };
+    });
+    console.log(orderSent);
+    const waiterId = auth.currentUser.uid;
+    const orderDate = firebase.firestore.FieldValue.serverTimestamp();
+    await db.collection('orders').add({
+      waiterId,
+      orderDate,
+      orderNumber: 1,
+      tableNumber: '',
+      orderItems: orderSent,
+      totalOrder: totalOrder,
+      status: 'order sent'
+    });
+  }
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const confirmedOrder = order.map((item) => (
+    <div key={item.id} className="food-items">
+      {`${item.name}`}
+      <div className="item-price">{item.qty > 1 ? item.qty + ' x ' : ''}{`$${item.price.toFixed(2)}`}</div>
+    </div>
+  ));
+
   return (
     <div className={showHideClassName}>
       <div className="modal">
         <div className="modal-content">
-        <div className="header"></div>
           <div className="info-container">
-            <div className="order-info">HOLA HOLA</div>
+            <h3 className="header">IS THE ORDER CORRECT?</h3>
+            <div className="order-info">
+              {confirmedOrder}
+            </div>
+            <div className="total">
+              <div className="total-info">TOTAL</div> 
+              <div className="total-number">${totalOrder}</div> 
+            </div>
           </div>
           <div className="footer">
             <button className="btn-no" onClick={onHide}>
               NO
             </button>
-            <button className="btn-yes">
+            <button className="btn-yes" onClick={() => sendOrder(order)}>
               YES
             </button>
-            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -4,6 +4,9 @@ import Modal from '../Modal/Modal';
 
 export default function Order(props) {
   const { order, removeFromOrder } = props;
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const totalValue = order.reduce((total, value) => total + value.qty * value.price, 0);
   const totalOrder = totalValue.toFixed(2);
 
@@ -13,11 +16,6 @@ export default function Order(props) {
       <div className="item-price">{item.qty > 1 ? item.qty + ' x ' : ''}{`$${item.price.toFixed(2)}`}</div>
     </div>
   ));
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <div className="order-container">
@@ -34,7 +32,7 @@ export default function Order(props) {
             ${totalOrder}</div>
           </div>
         </div>
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={handleClose} order={order} totalOrder={totalOrder}>
         </Modal>
         <button className="send-btn" onClick={handleShow}>SEND TO KITCHEN</button>
     </div>
