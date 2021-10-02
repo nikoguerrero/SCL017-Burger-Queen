c0858ce2c625e439ef9eee29874d6474bca09dbc
nikoguerrero <nclguerrero@gmail.com>
2021-10-02 16:55:43 -0300

    Filtering order items

--- src/components/Cook/Kitchen/Kitchen.jsx
+++ src/components/Cook/Kitchen/Kitchen.jsx
@@ -2,49 +2,29 @@ import React /*, { useEffect }*/ from 'react';
 import './Kitchen.css';
 import KitchenBar from '../KitchenBar/KitchenBar';
 import Tickets from '../Tickets/Tickets';
-// import { db } from '../../../firebase';
 
 const Kitchen = (props) => {
-  const { data, /*fetchData, status,*/ setStatus } = props;
+  const { data, status, setStatus } = props;
+  let filteredData = data;
 
-  // const fetchData = async (kitchenOrder) => {
-  //   const orderStatus = kitchenOrder;
-  //   const orderData = await orderStatus.get();
-  //   const array = [];
-  //   for (const doc of orderData.docs) {
-  //     // console.log(doc.id, '=>', doc.data());
-  //     const element = doc.data();
-  //     const id = doc.id;
-  //     array.push({...element, id});
-  //   }
-  //   setData(array);
-
-  // const fetchData = (kitchenOrder) => {
-  //   const orderStatus = kitchenOrder;
-  //   orderStatus.onSnapshot(snapshot => {
-  //     const array = [];
-  //     let changes = snapshot.docChanges();
-  //   // console.log(changes);
-  //     changes.forEach(change => {
-  //       //console.log(change.type);
-  //       if (change.type === 'added') {
-  //         const element = change.doc.data();
-  //         const id = change.doc.id;
-  //         array.push({...element, id});
-  //       } else if (change.type === 'removed') {
-  //         // array.push({});
-  //       }
-  //     });
-  //     setData(array);
-  //   });
-  // };
-
-  console.log(data);
+  switch (status) {
+    case 'new':
+      filteredData = data.filter((item) => item.status === 1);
+      break;
+    case 'active':
+      filteredData = data.filter((item) => item.status === 2);
+      break;
+    case 'done':
+      filteredData = data.filter((item) => item.status === 3);
+      break;
+    default:
+      break;
+  }
   
   return (
     <div className="kitchen-grid">
       <Tickets
-      data={data}
+      data={filteredData}
       />
       <KitchenBar 
       setStatus={setStatus}
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -11,21 +11,10 @@ const MainContent = () => {
   const [data, setData] = useState([]);
   const [status, setStatus] = useState('all orders');
 
-  // var unsubscribe;
-
   useEffect(() => {
     const orderCollection = db.collection('orders').orderBy('orderDate', 'desc');
     const unsuscribe = orderCollection.onSnapshot(snapshot => {
       const array = [];
-      // let changes = snapshot.docChanges();
-      // changes.forEach(change => {
-      //   //console.log(change.type);
-      //   if (change.type === 'added') {
-      //     const element = change.doc.data();
-      //     const id = change.doc.id;
-      //     array.push({...element, id});
-      //   }
-      // });
       snapshot.forEach(doc => {
         const element = doc.data();
         const id = doc.id;
@@ -35,45 +24,6 @@ const MainContent = () => {
     });
     return () => unsuscribe;
   }, []);
-  
-
-  const fetchData = (kitchenOrder) => {
-    const orderStatus = kitchenOrder;
-    
-  };
-
-  // useEffect(() => {
-  //   const orderCollection = db.collection('orders').orderBy('orderDate', 'desc');
-  //   switch (status) {
-  //     case 'all orders':
-  //       fetchData(orderCollection);
-  //     break;
-  //     case 'new':
-  //       fetchData(orderCollection.where('status', '==', 1));
-  //     break;
-  //     case 'active':
-  //       fetchData(orderCollection.where('status', '==', 2));
-  //     break;
-  //     case 'done':
-  //       fetchData(orderCollection.where('status', '==', 3));
-  //     break;
-  //     default:
-  //       // fetchData(orderCollection);
-  //     break;
-  //   }
-  // }, [status]);
-
-  const getData = async () => {
-    const orderStatus = db.collection('orders').where('status', '==', 3);
-    const orderData = await orderStatus.get();
-    const array = [];
-    for (const doc of orderData.docs) {
-      const element = doc.data();
-      const id = doc.id;
-      array.push({...element, id});
-    }
-    setData(array);
-  };
 
   return (
     <div className="general-grid">
@@ -82,7 +32,7 @@ const MainContent = () => {
           <img src= { title } alt="app name" className="img-title"></img>
         </div>
         <Link to="/serve" className="link-to-btn">
-          <button className="orders-ready" onClick={getData}>ORDERS READY</button>
+          <button className="orders-ready">ORDERS READY</button>
         </Link>
         <Link to="/menu" className="link-to-btn">
           <button className="orders-ready">BACK</button>
@@ -91,7 +41,6 @@ const MainContent = () => {
       </div>
       <Routes 
       data={data}
-      fetchData={fetchData}
       setData={setData}
       status={status}
       setStatus={setStatus}
